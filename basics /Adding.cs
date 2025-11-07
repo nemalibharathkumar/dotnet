775932ae2458723ba579cdb21800ae4d63a2a8ad
nemalibharathkumar <nemali.bharathkumar@gmail.com>
2025-11-07 19:38:29 +0530

    adding of two numbers

---
+++ basics /Adding.cs
@@ -0,0 +1,11 @@
+class program
+{
+  public static void Main()
+  {
+    int a,b;
+    a=int.Parse(System.console.ReadLine());
+    b=int.Parse(System.Console.ReadLine());
+    System.Console.WriteLine("Adding of two number:"+(a+b));
+  }
+}
+//the ouput of this program is:a+b
